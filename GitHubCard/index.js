35b3b47f23dbe8163f85522c9923b728fd019535
Jamie <jamielynjenks@gmail.com>
2020-12-03 17:21:20 -0700

    created axios request to see the data, and created card creator function

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,13 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/Lukerdue')
+  .then(res=>{
+    console.log(res.data);
+  })
+  .catch(drama=>{
+    console.log(drama);
+  })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
